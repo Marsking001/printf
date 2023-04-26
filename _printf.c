dd48d0f61f4c7ad3c3baa85f592e6c7e7de72746
temitope299 <125833328+temitope299@users.noreply.github.com>
2023-04-26 13:29:20 -0700

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -6,6 +6,7 @@ void print_buffer(char buffer[], int *buff_ind);
  * _printf - Printf function
  * @format: format.
  * Return: Printed chars.
+ *by: michael and nifemi
  */
 int _printf(const char *format, ...)
 {
